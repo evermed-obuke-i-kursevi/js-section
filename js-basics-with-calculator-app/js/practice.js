02b03f4faa56873b1ecb80743a89741e618e5609
m.bojkic <m.bojkic@emakina.rs>
2022-12-15 20:37:49 +0100

    22. cas (3. cas JS-a) - other data types, intro to objects and arrays, typeof operator

--- js-basics-with-calculator-app/js/practice.js
+++ js-basics-with-calculator-app/js/practice.js
@@ -295,3 +295,240 @@ function print() {
 let testingVar = 'Hello World!';
 
 print();
+
+
+// ! 3. cas JS-a (22. cas) (15.12.2022.)
+
+// Konverzija tipova podataka
+
+// * 1. eksplicitna konverzija tipova
+
+console.log('--------------- 3. cas JS-a (22. cas) (15.12.2022.) --------------');
+
+let t = 33 + '2';
+
+console.log(t); // 332 (string)
+
+let resVar = 33 + parseInt('2');
+
+console.log(`Result of adding is ${resVar}`);
+
+let resVar2 = 100 + parseFloat('50.5');
+
+console.log(`Decimal result of adding is ${resVar2}`);
+
+let resVar3 = 33 + +'2.5'; // * najbolji nacin za konverziju (plus predznakom)
+
+console.log(resVar3); // number (float)
+
+let resVar4 = resVar3.toString();
+
+console.log(resVar4); // string
+
+
+// * Implicitna konverzija tipova
+
+let resVar5 = 3 * '3';
+
+console.log(resVar5); // 9
+
+let resVar6 = '15' - 4;
+
+console.log(resVar6); // 11
+
+let resVar7 = 20 / '2';
+
+console.log(resVar7); // 10
+
+let resVar8 = 2**'2';
+
+console.log(resVar8); // 4
+
+
+// ! Kraci nacin pisanja operatora
+
+let resVar9 = resVar8 + 10;
+
+console.log(resVar9); // 14
+
+// resVar9 = resVar9 + 6; // ! duzi nacin
+
+resVar9 += 6; // * kraci nacin
+
+console.log(resVar9); // 20
+
+resVar9 -= 10;
+
+console.log(resVar9); // 10
+
+resVar9 *= 3;
+
+console.log(resVar9); // 30
+
+resVar9 /= 15;
+
+console.log(resVar9); // 2
+
+
+// Incerement and decrement
+
+let j = 1;
+let i = j++;
+
+console.log(`j: ${j} , i: ${i}`); // j: 2 , i: 1
+
+let k = 1;
+let n = ++k;
+
+console.log(`k: ${k} , n: ${n}`); // k: 2 , n: 2
+
+
+// Boolean --> true ili false ==> let isDone = true; // false
+
+// Object --> 
+
+/**
+ * const employee = {
+ *    name: 'Marko',
+ *    age: 28,
+ *    isStudent: false
+ * }
+ */
+
+// Arrays -->
+
+/**
+ * const numbers = [
+ *    1,2,3, '50', true, employee
+ * ]
+ */
+
+
+// ! Arrays
+
+let myArray = [];
+
+// myArray = [1,2,3,4,5];
+
+myArray.push(100);
+
+myArray.push(200);
+
+myArray.push(500);
+
+myArray.push(1000);
+
+myArray.push(400);
+
+myArray.push(20);
+
+console.log(myArray);
+
+console.log(myArray[0]);
+
+// console.log(myArray[10]); // ! undefined
+
+console.log(myArray[myArray.length-1]);
+
+
+// ! Objects
+
+const person = {
+   firstName: 'Marko',
+   lastName: 'Markovic',
+   age: 25,
+   gender: 'male',
+   isStudent: true,
+   favouriteBooks: ['Harry Potter', 'LOTR']
+}
+
+console.log(person.favouriteBooks);
+
+console.log(`Person's name is ${person.firstName} and last name is ${person.lastName}`); // Person's name is IME and last name is PREZIME
+
+console.log(`Person's gender is ${person['gender']}`);
+
+
+// ! Undefined, NULL, NaN
+
+let someVar;
+
+console.log(someVar); // undefined --> default vrednost za neinicijalizovane varijable
+
+
+let someVar2 = 55;
+
+someVar2 = null; // null --> gadja neku nepostojecu vrednost, i cesto se dodeljuje varijabli ako hocemo da resetujemo vrednost te varijable na nepostojecu vrednost.
+
+
+console.log('Welcome'-6); // NaN
+
+
+// ! TypeOf operator
+
+console.log(typeof(55)); // number
+
+console.log(typeof(100.42)); // number
+
+console.log(typeof(NaN)); // number
+
+console.log(typeof(`Neki string`)); // string
+
+console.log(typeof `Neki string`); // string
+
+console.log(typeof(true)); // boolean
+
+console.log(typeof(undefined)); // undefined
+
+console.log(typeof(null)); // object
+
+console.log(typeof(myArray)); // object
+
+console.log(typeof(person)); // object
+
+// TODO Homework (3. cas)
+
+/**
+ * DOMACI 3. cas:
+
+   1. Kreirati varijablu proizvoljnog naziva. U varijablu dodati vrednost koja se dobije tako sto saberu vrednosti 33 (tipa number) i ‘27’ (tipa string). Obezbediti konverziju (parsiranje) string-a u number kako bi se matematicka operacija uspesno izvrsila. Na kraju napraviti novu varijablu u koju cete dodati vrednost koja se dobija parsiranjem dobijenog number-a u string.
+   2. Prvu varijablu (tipa number) uvecati za 10 koristeci skraceni tip operatora. Nakon toga istu varijablu dekrementovati za jedan i ispisati konacnu vrednost na konzoli uz ispis ‘Current value is vrednost_varijable’.
+   3. Kreirati niz sa od sledecih elemenata: ‘hello’ , ‘planet’ , ‘Jupiter’. Na kraju izvrsiti konkatinaciju ovih stringova i rezultat ispisati na konzoli.
+   4. Kreirati objekat course i u njega dodati property-je: courseName (tipa string) i proizvoljnu vrednost, courseTeacher (tipa string) i proizvoljnu vrednost, duration (tipa number) i proizvoljnu vrednost i languages (tipa array) i proizvoljan niz elemenata.
+   5. Ispisati na konzoli kompletan niz elemenata (languages property) iz course objekta.
+   6. Ispisati poslednji element istog tog niza na konzoli.
+   7. Resetovati vrednost varijable iz prvog zadatka na praznu vrednost uz odgovarajuci tip podatka.
+   8. Proveriti tip podatka varijable iz prvog zadatka.
+
+ */
+
+let randomVar = 33 + +'27';
+
+let randomVarStr = randomVar.toString();
+
+randomVar += 10;
+
+randomVar--;
+
+console.log(`Current value is ${randomVar}`); // 69
+
+let testArray = ['Hello', 'planet', 'Jupiter'];
+
+console.log(`${testArray[0]} ${testArray[1]} ${testArray[2]}!`); // Hello planet Jupiter
+
+const course = {
+   courseName: 'Frontend Programming',
+   courseTeacher: 'Petar Petrovic',
+   duration: 1.5,
+   languages: ['JavaScript', 'HTML5', 'CSS3']
+}
+
+console.log(course.languages);
+
+// course['languages'];
+
+console.log(course.languages[course.languages.length-1]); // 'CSS3'
+
+randomVar = null;
+
+console.log(typeof randomVar);
