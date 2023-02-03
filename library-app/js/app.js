fa60a32f32f658e7e747ff174002a32da658b7ac
m.bojkic <m.bojkic@emakina.rs>
2023-02-03 09:42:30 +0100

    13. cas JS-a (32. cas) - library app homework

--- library-app/js/app.js
+++ library-app/js/app.js
@@ -26,15 +26,35 @@ const findBook = (userInput) => {
         .split(' ')
         .join(''); // '    HeAD HUNTERS   ' --> '    head hunters   ' --> 'head hunters' --> ['head', 'hunters'] --> 'headhunters'
     
-    const foundBook = libraryBooks.find(book => {
-        return book.name.toLowerCase().trim().split(' ').join('') === searchTerm;
-    });
+    // 4. zadatak
+    if(!searchTerm) {
+        alert('You must provide some search value!');
+        return;
+    }
 
-    alert(JSON.stringify(foundBook));
+    // ! STARA VERZIJA KODA --> ovde smo radili pretragu knjige striktno po nazivu
+    // const foundBook = libraryBooks.find(book => {
+    //     return book.name.toLowerCase().trim().split(' ').join('') === searchTerm;
+    // });
+
+    // alert(JSON.stringify(foundBook));
+
+    // ! 7. zadatak -> NOVA VERZIJA KODA --> ovde se radi pretraga svih knjiga na osnovu searchTerm-a (knjige koje sadrze u imenu zadati string)
+    const foundBooks = libraryBooks.filter(book => {
+        const partsOfName = book.name.toLowerCase().trim().split(' '); // 'Head Hunters' --> ['head','hunters']
+        const books = partsOfName.includes(searchTerm);
+        return books;
+    }); 
+    if(!foundBooks.length) {
+        alert(`There is no book in our library that contains provided search term: ${userInput.trim()}!`);
+        return;
+    }
+    alert(JSON.stringify(foundBooks, null, 5));
 }
 
 searchBookBtn.addEventListener('click', () => {
     findBook(searchBookInput.value);
+    clearFields();
 });
 
 
@@ -45,8 +65,22 @@ const addBook = (name, writter, attributeName = '', attributeValue = '') => {
         name, // skraceno od name: name
         writter
     }
+
+     // 5. zadatak
+    const bookExists = libraryBooks.some(book => 
+        book.name.toLowerCase().trim().split(' ').join('') === addedBook.name.toLowerCase().trim().split(' ').join(''));
+    if(bookExists) {
+        alert(`Book with provided name ${name.trim()} already exists!`);
+        return;
+    }
     if (attributeName !== '' && attributeValue !== '') {
         addedBook[attributeName] = attributeValue;
+    }
+
+    // 4. zadatak
+    if(!name.toLowerCase().trim().split(' ').join() || !writter.toLowerCase().trim().split(' ').join()) {
+        alert('You must provide some value!');
+        return;
     }
     libraryBooks.push(addedBook);
     console.log(libraryBooks);
@@ -55,6 +89,7 @@ const addBook = (name, writter, attributeName = '', attributeValue = '') => {
 
 addBookBtn.addEventListener('click', () => {
     addBook(bookNameInput.value, bookWritterInput.value, additionalAttributeNameInput.value, additionalAttributeValueInput.value);
+    clearFields();
 });
 
 
@@ -65,6 +100,12 @@ const deleteBook = (userInput) => {
         .trim()
         .split(' ')
         .join(''); // lord of THE RINGS --> lordoftherings
+
+    // 4. zadatak
+    if(!searchTerm) {
+        alert('You must provide some search value!');
+        return;
+    }
 
     const foundBook = libraryBooks.find(book => {
         return book.name.toLowerCase().trim().split(' ').join('') === searchTerm;
@@ -80,9 +121,35 @@ const deleteBook = (userInput) => {
 
 deleteBookBtn.addEventListener('click', () => {
     deleteBook(deleteBookInput.value);
+    clearFields();
 });
 
 
+// TODO SEE ALL funkcija
+// 3. zadatak
+seeAllBtn.addEventListener('click', () => {
+    // 1. zadatak
+    if(!libraryBooks.length) {
+        alert(`There are no books in our library at the moment. Please try again later!`);
+        return;
+    }
+    alert(`Check your console 📚`);
+    // 6. zadatak 
+    libraryBooks.sort((a,b) => {
+        return a.name.localeCompare(b.name);  // jos jedan nacin za sortiranje, uz pomocu localeCompare funkcije 
+    });
+    console.log(libraryBooks);
+});
+
+// 2. zadatak
+const clearFields = () => {
+    deleteBookInput.value = '';
+    bookNameInput.value = '';
+    bookWritterInput.value = '';
+    searchBookInput.value = '';
+    additionalAttributeValueInput.value = '';
+    additionalAttributeNameInput.value = '';
+}
 
 // TODO 32. cas domaci
 /*
